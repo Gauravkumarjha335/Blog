2d48f12b2422131637f3c228e08799ded2c22533
Gaurav Kumar Jha <94332256+Gauravkumarjha335@users.noreply.github.com>
2024-03-24 10:59:51 +0530

    configure express and mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,12 @@
-hidfgh
+import express from 'express'
+import mongoose from 'mongoose';
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+// Connect to MongoDB database
+
+mongoose.connect("mongodb+srv://Gaurav:Gaurav@atlascluster.bfkgaz9.mongodb.net/Blog?retryWrites=true&w=majority&appName=AtlasCluster")
+    .then(() => console.log('Connected to Database'))
+// .catch((err) => console.error(err));
