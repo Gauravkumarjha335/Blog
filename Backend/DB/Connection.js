35e09422fea7c8de910e83a63fafb400cb849413
Gaurav Kumar Jha <94332256+Gauravkumarjha335@users.noreply.github.com>
2024-03-26 12:53:06 +0530

    setup in global

--- Backend/DB/Connection.js
+++ Backend/DB/Connection.js
@@ -1,7 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-function connection() {
-    
+async function connectDB() {
+    try {
+
+        mongoose.connect("mongodb+srv://Gaurav:Gaurav@atlascluster.bfkgaz9.mongodb.net/Blog?retryWrites=true&w=majority&appName=AtlasCluster")
+            .then(() => console.log('Connected to Database'))
+        // .catch((err) => console.error(err));
+    } catch {
+        console.error(err);
+    }
 }
-
-export {connection};
+export default  connectDB;
--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,9 @@
 import express from 'express'
-import mongoose from 'mongoose';
+import connectDB from './DB/Connection.js';
 const PORT = 8000;
-
 const app = express();
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 // Connect to MongoDB database
 
-mongoose.connect("mongodb+srv://Gaurav:Gaurav@atlascluster.bfkgaz9.mongodb.net/Blog?retryWrites=true&w=majority&appName=AtlasCluster")
-    .then(() => console.log('Connected to Database'))
-// .catch((err) => console.error(err));
+connectDB();
