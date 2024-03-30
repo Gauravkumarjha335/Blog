e36103e115450dc6669e0cb5702b2bc284ab3e1d
Gaurav Kumar Jha <94332256+Gauravkumarjha335@users.noreply.github.com>
2024-03-30 16:35:42 +0530

    Vit Config

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <>
+     
+      <Home/>
+    </>
+  );
+}
+
+export default App;
